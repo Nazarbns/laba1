916ee35f2383646216c728d1e0393d621acd170e
Nazar <bunnikn@mail.ru>
2023-11-05 17:11:49 +0300

    1.3

--- lab_1.3(2).py
+++
@@ -1,6 +0,0 @@
-x=int(input("Długość: "))
-y=int(input("Szerokość: "))
-S=x*y
-print(f"Eśli długość ({x}) i szerokość ({y}), wtedy Obszar prostokąta: ",S)
-P=(x+y)*2
-print(f"Eśli długość ({x}) i szerokość ({y}), wtedy Obwód prostokąta: ",P)
