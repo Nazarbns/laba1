b0dbe3d679e9a7a7104de9f99bf3f87b928419b3
Nazar <bunnikn@mail.ru>
2024-02-06 01:50:52 +0300

    1.3

---
+++ lab7/lab_7(e).py
@@ -0,0 +1,31 @@
+import pandas as pd
+data = {'Imię': ['Robert', 'Magda', 'Tomasz', 'Zosia'],
+        'nazwisko': ['Nowak', 'Kowalska', 'Lewandoski', 'Szpilewski'],
+        'nr_albumu': [70532, 70256, 70712, 70402],
+        'ocena_z_kolokwium': [4.0, 3.0, 5.0, 3.5]}
+data1 = {'nr_albumu': [70593, 70429, 70237, 70594],
+        'ocena_z_kolokwium_popr': [4, 5, 4.5, 3.5]}
+df = pd.DataFrame(data)
+df1 = pd.DataFrame(data1)
+# a.
+oceny_wieksze_niz_4 = df[df['ocena_z_kolokwium'] > 4]
+
+# b.
+posortowani_studenci = df.sort_values(by='ocena_z_kolokwium')
+
+srednia_ocena_w_grupach = df.groupby('ocena_z_kolokwium')['ocena_z_kolokwium'].mean()
+
+merged_df = pd.merge(df, df1, on='nr_albumu', how='left')
+
+print(df)
+
+print("\nStudenci z oceną większą niż 4:")
+print(oceny_wieksze_niz_4)
+print("\nStudenci posortowani według ocen:")
+print(posortowani_studenci)
+
+print("\nŚrednia ocena w każdej grupie:")
+print(srednia_ocena_w_grupach)
+
+print("\nPołączona ramka danych:")
+print(merged_df)
---
+++ lab7/lab_7(i).py
@@ -0,0 +1,49 @@
+import pandas as pd
+data = {'Imię': ['Robert', 'Magda', 'Tomasz', 'Zosia'],
+        'nazwisko': ['Nowak', 'Kowalska', 'Lewandoski', 'Szpilewski'],
+        'nr_albumu': [70532, 70256, 70712, 70402],
+         'ocena_z_kolokwium': [4, 3.5, 4, 3]}
+data1 = {'nr_albumu': [70593, 70429, 70237, 70594],
+        'ocena_z_kolokwium_popr': [4.5, 5, 4.5, 3.5]}
+
+df = pd.DataFrame(data)
+df1 = pd.DataFrame(data1)
+
+oceny_wieksze_niz_4 = df[df['ocena_z_kolokwium'] > 4]
+
+posortowani_studenci = df.sort_values(by='ocena_z_kolokwium')
+
+srednia_ocena_w_grupach = df.groupby('ocena_z_kolokwium')['ocena_z_kolokwium'].mean()
+
+merged_df = pd.merge(df, df1, on='nr_albumu', how='left')
+
+merged_df.to_csv('merged_data.csv')
+
+loaded_df = pd.read_csv('merged_data.csv')
+
+unikalne_oceny = df['ocena_z_kolokwium'].unique()
+
+liczba_ocen_5 = df1['ocena_z_kolokwium_popr'].value_counts().get(5)
+
+print(df)
+
+print("\nStudenci z oceną większą niż 4:")
+print(oceny_wieksze_niz_4)
+print("\nStudenci posortowani według ocen:")
+print(posortowani_studenci)
+
+print("\nŚrednia ocena w każdej grupie:")
+print(srednia_ocena_w_grupach)
+
+print("\nPołączona ramka danych:")
+print(merged_df)
+
+print("\nPlik CSV został pomyślnie zapisany.")
+
+print("\nWczytane dane:")
+print(loaded_df.head())
+
+print("\nUnikalne oceny:")
+print(unikalne_oceny)
+
+print("\nLiczba studentów z oceną równą 5:", liczba_ocen_5)
